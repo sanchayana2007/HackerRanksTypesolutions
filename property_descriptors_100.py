824e794bacca5cc63fb60ef086091b8e4bbb655b
sanchayana2007 <sanchayana2007@gmail.com>
2016-06-29 11:08:46 +0530

    descriptor

---
+++ property_descriptors_100.py
@@ -0,0 +1,37 @@
+__author__ = 'Sanchayan'
+
+
+class ProprtyDescriptor:
+#Put a class atribute to used as a property
+    _x=0
+
+    def __init__(self):
+        print('Initilising the Atribute')
+        self._x ='intilised'
+
+
+    @property
+    def x(self):
+        print("I am in Getter Property")
+
+        return self._x
+
+    @x.setter
+    def x(self,value):
+        print("Inside the Setter")
+        self._x = value
+
+    @x.deleter
+    def x(self):
+        print('Inside Deleter')
+        del self._x
+
+testProperty = ProprtyDescriptor()
+testProperty.x = 'Set'
+testProperty.x
+
+del testProperty.x
+
+
+
+
