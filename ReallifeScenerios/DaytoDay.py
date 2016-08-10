ebbbbcc4a15edbe47cbf38d2fbf1e1b9f4ae1174
sanchayana2007 <sanchayana2007@gmail.com>
2016-08-10 20:27:56 +0530

    String repalcement

--- ReallifeScenerios/DaytoDay.py
+++ ReallifeScenerios/DaytoDay.py
@@ -1,1 +1,10 @@
 
+##Problem 1 : Splicing second occurence of a String 
+''' Deccription : S="12abc6789abcdefgh" needs to be 12abc6789###defgh but with str.replace(str[9:11], "###") It Produces 
+12###6789###defgh'''
+
+##Solution 
+S="12abc6789abcdefgh"
+S=("").join(S[:9],"###",S[12:]
+
+
