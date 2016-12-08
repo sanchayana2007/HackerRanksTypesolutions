f271d35b11362a646d4d6c8baf71097c9a972567
sanchayana2007 <sanchayana2007@gmail.com>
2016-12-08 22:02:47 +0530

    Win checked file

--- Automation/Pickel_DB.py
+++ Automation/Pickel_DB.py
@@ -1,68 +1,85 @@
 __author__ = 'Sanchayan'
 import pickle
-P={}
-class Password_Store(object):
-	def __init__(self,Servername,passwords_list):
-		self.Servername = Servername
-		self.password_list=passwords_list
-		
-	def append_password(self,password):
-		if password in password_list:
-			
-			return False
-		else:
-			password_list.append(password)
-			return True	
 
-
-
-def Populate_Server_passwords():
-	try :
-		with open("Passwords.p","rb") as source:
-			servers_present= pickle.load(source)
-			
-			#for server in servers_present:
-			#	P[server.Servername]=Password_Store(server.Servername,server.password_list)
-			P=	servers_present
-			print('Populate_Server_passwords',P)
-		
-	except IOError:
-				print('password keeping file is deleted or You dont havent kept it as Now')
+#basic functions for Reading , Writing data by pickel
 		
 		
+class table(object):
+	def __init__(self,name):
+		self.P={}
+		self.name = name
+		self.Populate_Server_passwords()
 	
-
-def Write_Server_info(servername,password):
-	if len(P) != 0:
-		
-		if servername in P :
-			print('Serevr file is present',servername)
-			if password not in P[servername]:
-				P[servername].append(password)
-				print("password not Present")
-			else: 
+	
+	def Populate_Server_passwords(self):
+		try :
+			filename=self.name + '.p'
+			with open(filename,"rb") as source:
+				servers_present= pickle.load(source)
+				
+				#for server in servers_present:
+				#	P[server.Servername]=Password_Store(server.Servername,server.password_list)
+				self.P=	servers_present
+				#print('Populate_Server_passwords',P)
 			
-				print("password  Present")
-		else:
-			print('Serevr is new entry ',servername)
-			P[servername]=[password,]
-	else:	
-		P[servername]=[password,]
+		except IOError:
+					print('Table is created:',self.name)
+	
+	
+	def add_data(self,rowid,data):
+		#Write_Server_info(rowid,data,self.name,self.P)
+		filename= self.name + '.p'
 
-	with open('Passwords.p','wb') as target:
-		pickle.dump(P,target)
+		if len(self.P) != 0:
+			
+			if rowid in self.P :
+				print('rowid file is present',rowid)
+				if data not in self.P[rowid]:
+					self.P[rowid].append(data)
+					#print("password not Present")
+				else: 
+				
+					print("data  Present")
+			else:
+				print('rowid is new entry ',rowid)
+				self.P[rowid]=[data,]
+		else:	
+			self.P[rowid]=[data,]
+	
+		with open(filename,'wb') as target:
+			pickle.dump(self.P,target)
+		
+	def read_data(self):
+		print('table ',self.name,self.P)
+
 
 	
 if __name__== '__main__':
-	Populate_Server_passwords()
-	'''
-	Write_Server_info('1.com','11')
-	Write_Server_info('12.com','112')
-	Write_Server_info('12.com','113')
-	Write_Server_info('12.com','115')
-	Write_Server_info('13.com','113')
-	Write_Server_info('14.com','114')
-	'''
-
-    # The pickel object Load will produce the same as normal object
+	t=table("Passwords")
+	
+	t.add_data('1.com','113')
+	t.add_data('1.com','112')
+	t.add_data('2.com','131')
+	t.add_data('3.com','114')
+	t.add_data('3.com','114')
+	t.read_data()
+	
+	t1=table("Passwords1")
+	
+	t1.add_data('1.com','113')
+	t1.add_data('1.com','112')
+	t1.add_data('2.com','131')
+	t1.add_data('3.com','114')
+	t1.read_data()
+	
+	t2=table("Food")
+	
+	t2.add_data('veg','Allo')
+	t2.add_data('NonVeg','chicken')
+	t2.add_data('veg','patal')
+	t2.add_data('veg','achar')
+	t2.add_data('NonVeg','fish')
+	t2.read_data()
+	
+	
     
