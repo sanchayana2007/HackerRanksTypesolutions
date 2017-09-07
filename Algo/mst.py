0cbe80197dcaa1d2374b2c340a147968117864d8
sanchayana2007 <sanchayana2007@gmail.com>
2017-09-07 10:54:50 +0530

    wqwwqwqw
    ewe

--- Algo/mst.py
+++ Algo/mst.py
@@ -9,61 +9,19 @@ class Graph(object):
         if vertex2 not in self.g:
             self.g[vertex2] = {}
 
-        self.g[vertex1][vertex2] = weight
-        self.g[vertex2][vertex1] = weight
-
-    def has_link(self, v1, v2):
-        return v2 in self[v1] or v1 in self[v2]
+        self.g[vertex1][vertex2] = self.g[vertex2][vertex1] = weight
 
     def edges(self):
         data = []
-
         for from_vertex, destinations in self.g.items():
             for to_vertex, weight in destinations.items():
+		print("Iems dest",destinations.items())
                 if (to_vertex, from_vertex, weight) not in data:
                     data.append((from_vertex, to_vertex, weight))
-
         return data
 
     def sorted_by_weight(self, desc=False):
         return sorted(self.edges(), key=lambda x: x[2], reverse=desc)
-
-    def spanning_tree(self, minimum=True):
-        mst = Graph()
-        parent = {}
-        rank = {}
-
-        def find_parent(vertex):
-            while parent[vertex] != vertex:
-                vertex = parent[vertex]
-
-            return vertex
-
-        def union(root1, root2):
-            if rank[root1] > rank[root2]:
-                parent[root2] = root1
-            else:
-                parent[root2] = root1
-
-                if rank[root2] == rank[root1]:
-                    rank[root2] += 1
-
-        for vertex in self.g:
-            parent[vertex] = vertex
-            rank[vertex] = 0
-
-        for v1, v2, weight in self.sorted_by_weight(not minimum):
-            parent1 = find_parent(v1)
-            parent2 = find_parent(v2)
-
-            if parent1 != parent2:
-                mst.add(v1, v2, weight)
-                union(parent1, parent2)
-
-            if len(self) == len(mst):
-                break
-
-        return mst
 
     def __len__(self):
         return len(self.g.keys())
@@ -77,6 +35,43 @@ class Graph(object):
 
     def __str__(self):
         return "\n".join('from %s to %s: %d' % edge for edge in self.edges())
+
+def spanning_tree(self, minimum=True):
+    mst = Graph()
+    parent = {}
+    rank = {}
+
+    def find_parent(vertex):
+        while parent[vertex] != vertex:
+            vertex = parent[vertex]
+
+            return vertex
+
+    def union(root1, root2):
+        if rank[root1] > rank[root2]:
+            parent[root2] = root1
+        else:
+            parent[root2] = root1
+
+        if rank[root2] == rank[root1]:
+            rank[root2] += 1
+
+        for vertex in self.g:
+            parent[vertex] = vertex
+            rank[vertex] = 0
+
+        for v1, v2, weight in self.sorted_by_weight(not minimum):
+            parent1 = find_parent(v1)
+            parent2 = find_parent(v2)
+
+            if parent1 != parent2:
+               mst.add(v1, v2, weight)
+               union(parent1, parent2)
+
+            if len(self) == len(mst):
+               break
+
+        return mst
 
 graph = Graph()
 
@@ -100,7 +95,8 @@ graph.add('d', 'f', 7)
 graph.add('f', 'd', 7)
 graph.add('f', 'e', 8)
 graph.add('f', 'g', 9)
-
-print(graph.spanning_tree())
+print("Graph items ",graph.g)
+print("Graph edges ",graph.edges())
+#print(graph.spanning_tree())
 print()
-print(graph.spanning_tree(False))s
+#print(graph.spanning_tree(False))
